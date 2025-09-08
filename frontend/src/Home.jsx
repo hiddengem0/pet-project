48474208cdc50980c57947d691885094681b5dfa
hiddengem0 <hiddengem.0@outlook.com>
2025-09-08 01:14:25 +0100

    edit

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -5,17 +5,33 @@ import "./Home.css";
 
 function Home() {
   const[currentIndex, setCurrentIndex] = useState(0);
+  const[ShowInfo, setShowInfo] = useState(false);
+  const[favourites, setfavourites] = useState([]);
+  
+ const toggleinfo = () => {
+    setShowInfo=((prev) => !prev );
+  };
+
+  const addfavourites = () => {
+    const petName = currentProfile.name;
+    if (!favourites.includes(petName)) {
+        setfavourites([favourites, petName])
+    }
+  }
 
   const Next = () => {
     setCurrentIndex((prevIndex) => 
       prevIndex >= petProfiles.length - 1 ? 0 : prevIndex + 1
     );
+    setShowInfo(False)
   };
 
   const Previous = () => {
     setCurrentIndex((prevIndex) => prevIndex === 0 ? petProfiles.length - 1 : prevIndex - 1
     );
+    setShowInfo(False)
   };
+
   const currentProfile = petProfiles[currentIndex];
 
   return (
@@ -26,10 +42,10 @@ function Home() {
             src={currentProfile.image}
             alt={currentProfile.name}
             className="profile-img"
+            onClick={toggleinfo}
+            style={{ cursor: "pointer" }}
           />
-          <div className="profile-name">{currentProfile.name}</div>
         </div>
-
         <div className="button-group">
           <button onClick={Previous}>Previous</button>
           <button onClick={Next}>Next</button>
@@ -40,8 +56,20 @@ function Home() {
         <h2>{currentProfile.name}</h2>
         <p>{currentProfile.info}</p>
       </div>
+
+      <div className="favourite">
+        <button onClick={addfavourites}>Favourite</button>
+      </div>
     </div>
   );
 }
 
 export default Home;
+
+// --- plans --- 
+// add a fave button top right of img 
+// add localstorage for favourited format 
+// home page 
+// add wallpaper for welcome and reformat
+// create profile page
+
