f21c829efbd86e897243cc96c7f2e5a35ab3ac11
hiddengem0 <hiddengem.0@outlook.com>
2025-09-24 23:48:55 +0100

    finishing all home functions

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,40 +1,51 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import petProfiles from "./data/petprofiles";
 import "./Home.css";
 
 function Home() {
   const[currentIndex, setCurrentIndex] = useState(0);
   const[ShowInfo, setShowInfo] = useState(false);
-  const[favourites, setfavourites] = useState([]);
+
+  const[favourites, setfavourites] = useState(() => { // ui update
+    const saved = localStorage.getItem("favourites"); 
+    return saved ? JSON.parse(saved) : []; // returns info or nothing, returning [] prevents crash from null
+  });
+
+  useEffect(() => {
+    localStorage.setItem("favourites", JSON.stringify(favourites));
+  }, [favourites]);
   
  const toggleinfo = () => {
-    setShowInfo=((prev) => !prev );
+    setShowInfo((prev) => !prev );
   };
 
-  const addfavourites = () => {
+  const togglefav = () => {
     const petName = currentProfile.name;
-    if (!favourites.includes(petName)) {
-        setfavourites([favourites, petName])
-    }
+    if (favourites.includes(petName)) {
+        setfavourites(favourites.filter((name) => name !==petName)); //clear
+    } else {
+        setfavourites([...favourites, petName]); //add
+     } 
   }
 
   const Next = () => {
     setCurrentIndex((prevIndex) => 
       prevIndex >= petProfiles.length - 1 ? 0 : prevIndex + 1
     );
-    setShowInfo(False)
+    setShowInfo(false)
   };
 
   const Previous = () => {
     setCurrentIndex((prevIndex) => prevIndex === 0 ? petProfiles.length - 1 : prevIndex - 1
     );
-    setShowInfo(False)
+    setShowInfo(false)
   };
 
   const currentProfile = petProfiles[currentIndex];
+  const isFavourite = favourites.includes(currentProfile.name); // checks if already in favourites
 
-  return (
+    return (
     <div className="home-container">
       <div className="profile-section">
         <div className="profile-card">
@@ -45,21 +56,23 @@ function Home() {
             onClick={toggleinfo}
             style={{ cursor: "pointer" }}
           />
+          <button className="favbutton" onClick={togglefav}>
+            {isFavourite ? "❤️" : "🤍"}
+          </button>
         </div>
+
         <div className="button-group">
           <button onClick={Previous}>Previous</button>
           <button onClick={Next}>Next</button>
         </div>
       </div>
 
-      <div className="profile-details">
-        <h2>{currentProfile.name}</h2>
-        <p>{currentProfile.info}</p>
-      </div>
-
-      <div className="favourite">
-        <button onClick={addfavourites}>Favourite</button>
-      </div>
+      {ShowInfo && (
+        <div className="profile-details">
+          <h2>{currentProfile.name}</h2>
+          <p>{currentProfile.info}</p>
+        </div>
+      )}
     </div>
   );
 }
@@ -67,9 +80,7 @@ function Home() {
 export default Home;
 
 // --- plans --- 
-// add a fave button top right of img 
-// add localstorage for favourited format 
-// home page 
+// reformat home page 
 // add wallpaper for welcome and reformat
 // create profile page
 
